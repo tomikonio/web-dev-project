e7a858dde68d5a939218a729e01abc9eaa24f894
Igor <igrr69@gmail.com>
2018-07-26 19:53:35 +0300

    Add input and click detection to ImageLinkForm, notify the App component

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">
@@ -9,8 +9,11 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className="form pa4 br4 shadow-5">
-          <input className="f4 pa2 w-70" type="text" />
-          <button className="w-30 f4 grow link pv2 ph3 pv2 dib white bg-light-purple">
+          <input className="f4 pa2 w-70" type="text" onChange={onInputChange} />
+          <button
+            className="w-30 f4 grow link pv2 ph3 pv2 dib white bg-light-purple"
+            onClick={onButtonSubmit}
+          >
             Detect
           </button>
         </div>
