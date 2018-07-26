5bdc4433ffc25a35d7f058878449932d53c2123a
Igor <igrr69@gmail.com>
2018-07-26 13:46:17 +0300

    Complete particles part

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
